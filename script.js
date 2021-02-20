8a05b288b171fea6af22dedcbe36aaa4ffefd4c6
James Ryan <james.ryan@pathwayforcare.com>
2021-02-20 17:38:20 +0000

    Added init function

--- script.js
+++ script.js
@@ -11,14 +11,27 @@ const btnHold = document.querySelector('.btn--hold');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
+let currentScore, activePlayer, totalScores, playing;
+
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-let currentScore = 0;
-let activePlayer = 0;
-let totalScores = [0, 0];
-let playing = true;
+const init = () => {
+  currentScore = 0;
+  activePlayer = 0;
+  totalScores = [0, 0];
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentScore0El.textContent = 0;
+  currentScore1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--active');
+  player1El.classList.remove('player--winner');
+};
+init();
 
 const switchActivePlayer = () => {
   document.querySelector(`.current--${activePlayer}`).textContent = 0;
@@ -77,18 +90,4 @@ btnHold.addEventListener('click', () => {
   }
 });
 
-btnNew.addEventListener('click', () => {
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  currentScore0El.textContent = 0;
-  currentScore1El.textContent = 0;
-  currentScore = 0;
-  activePlayer = 0;
-  totalScores = [0, 0];
-  playing = true;
-  diceEl.classList.add('hidden');
-  player0El.classList.add('player--active');
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--active');
-  player1El.classList.remove('player--winner');
-});
+btnNew.addEventListener('click', init);
