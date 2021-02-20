aea4be58eafe714efc26d35a23af8497f034e80f
James Ryan <james.ryan@pathwayforcare.com>
2021-02-20 08:16:30 +0000

    Added starting conditions

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const score0El = document.querySelector('.score--0');
+const currentScore0El = document.querySelector('.current--0');
+const score1El = document.querySelector('.score--1');
+const currentScore1El = document.querySelector('.current--1');
+const diceEl = document.querySelector('.dice');
+const hide = el => el.classList.add('hidden');
+const show = el => el.classList.remove('hidden');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+hide(diceEl);
